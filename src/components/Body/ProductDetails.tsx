d99670b86f0c60b310d5231a4a2519ada3bb0f30
anup-an <friek_an@yahoo.com>
2020-12-30 04:14:46 +0200

    Added product page and checkout form as modals with animation

---
+++ src/components/Body/ProductDetails.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+interface IProduct {
+    id: string;
+    title: string;
+    image: string;
+    description: string;
+    price: number;
+    availableSizes: string[];
+    count?: number;
+}
+
+interface IProps {
+    product: IProduct | null;
+    addToCart: (product: IProduct) => void;
+}
+
+const ProductDetails: React.FC<IProps> = ({ product, addToCart }) => {
+    return (
+        <div>
+            {product ? (
+                <div className="flex flex-row space-x-10 items-center">
+                    <img className="w-1/3 h-auto" src={`${product?.image}`} loading="eager" alt={`${product?.title}`} />
+                    <div className="flex flex-col space-y-6">
+                        <div className="text-lg font-bold">{product?.title}</div>
+                        <div>{product?.description}</div>
+                        <div className="flex flex-row justify-between">
+                            <div className="flex flex-row space-x-2 items-center">
+                                <div className="text-md">Available Sizes:</div>
+                                {product?.availableSizes.map((size) => (
+                                    <div key={size} className="p-2 text-xl border bg-gray-200 w-14 text-center">
+                                        {size}
+                                    </div>
+                                ))}
+                            </div>
+
+                            <button
+                                type="button"
+                                onClick={() => addToCart(product)}
+                                className="bg-blue-400 hover:bg-blue-800 text-white p-2 border rounded"
+                            >
+                                Add to Cart
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                ''
+            )}
+        </div>
+    );
+};
+
+export default ProductDetails;
