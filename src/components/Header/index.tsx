0b2c661cc0a7635204b6ba14aa38c75010f533d4
anup-an <friek_an@yahoo.com>
2021-08-27 12:09:12 +0300

    UX fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -71,7 +71,7 @@ class Header extends React.Component<IProps, IState> {
                             d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                         />
                     </svg>
-                </div>
+                </Link>
                 <Link to="/" className="hidden lg:block border border-blue-800 focus:border-white p-2">
                     Home
                 </Link>
