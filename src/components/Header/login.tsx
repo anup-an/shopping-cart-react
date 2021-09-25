361714c9406447daeac402f345de3641822524b7
anup-an <friek_an@yahoo.com>
2021-09-26 00:38:08 +0300

    login error message display testing

--- src/components/Header/login.tsx
+++ src/components/Header/login.tsx
@@ -35,7 +35,7 @@ class Login extends React.Component<IProps, IState> {
     handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
         event?.preventDefault();
         const errorMessage = this.props.actions.logInUser(this.state.email, this.state.password, this.props.cartItems);
-        errorMessage.then(err => this.setState({ ...this.state, error: err }));
+        errorMessage.then(err => this.setState({ ...this.state, error: 'error' }));
 
     };
     render(): JSX.Element {
