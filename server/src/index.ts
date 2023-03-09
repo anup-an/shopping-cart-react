f59f04226458df54fa83f99c9d02bdb1f607ad01
Anup Poudel <anup.poudel@ambine.com>
2023-03-09 23:07:33 +0200

    Rearrange middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,19 +12,18 @@ import strategy from './middlewares/passport';
 require('dotenv').config();
 
 const app = express();
-app.use(cors({ credentials: true, origin: true }));
 app.use(express.urlencoded({ extended: false }));
-app.use(function (req, res, next) {
+
+app.use(express.json());
+
+app.use(cookieParser());
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://lucid-lewin-704e07.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
-
-app.use(express.json());
-
-app.use(cookieParser());
 
 const mongoUri =
     process.env.username && process.env.password
