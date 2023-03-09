304a6f2523983e138892b9d03cf65fd4e979701d
Anup Poudel <anup.poudel@ambine.com>
2023-03-10 00:01:19 +0200

    Update cors issue handling middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,7 +31,7 @@ app.use(passport.initialize());
 
 passport.use(strategy);
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://lucid-lewin-704e07.netlify.app'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
