3bd0eba9bca9b699faf7d3df0a4c3597a57b8453
anup-an <friek_an@yahoo.com>
2023-03-22 11:44:18 +0200

    Update cors origin list

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,49 +12,27 @@ import strategy from './middlewares/passport';
 require('dotenv').config();
 
 const app = express();
-
-app.use(
-    cors({
-        credentials: true,
-        origin: [
-            'https://lucid-lewin-704e07.netlify.app',
-            'https://shopping-cart-react-express.onrender.com',
-            /http\:\/\/localhost\:\d+/,
-        ],
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-        allowedHeaders: [
-            'Content-Type',
-            'Origin',
-            'X-Requested-With',
-            'Accept',
-            'x-client-key',
-            'x-client-token',
-            'x-client-secret',
-            'Authorization',
-        ],
-    }),
-);
+app.use(cors({ credentials: true, origin: 'https://lucid-lewin-704e07.netlify.app' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use(cookieParser());
 
-const mongoUri =
-    process.env.username && process.env.password
-        ? `mongodb+srv://${process.env.username}:${process.env.password}@cluster0.f4upd.mongodb.net/shopping-cart-db?retryWrites=true&w=majority`
-        : '';
-mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(
+    'mongodb+srv://anup-an:IvNT46LZr37Vz0IN@cluster0.f4upd.mongodb.net/shopping-cart-db?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    },
+);
 
 app.use(passport.initialize());
-
 passport.use(strategy);
 app.get('/', (req, res) => res.send('This is the server homepage'));
 app.use(routes());
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening to http://localhost:${port}`);
 });
