10ae0e1bae728b89a91afe652003d593e788158e
anup-an <friek_an@yahoo.com>
2023-04-07 07:27:44 +0300

    Add test for routes to find and delete products

--- server/tests/e2e/products.test.ts
+++ server/tests/e2e/products.test.ts
@@ -5,20 +5,22 @@ import app from '../../src';
 import { products, registerPayload, testProduct } from '../testData';
 
 describe('GET /api/products', () => {
-    let db: mongoose.Connection
-    let cookie: string[]
+    let db: mongoose.Connection;
+    let cookie: string[];
 
-    beforeAll(async() => {
-        db = mongoose.connection
-        await db.collections.products.insertMany(products)
+    beforeAll(async () => {
+        db = mongoose.connection;
+        await db.collections.products.insertMany(products);
         await request(app).post('/api/signup').send(registerPayload);
-        const loginResponse = await request(app).post('/api/login').send({ email: 'anup.poudel@ambine.com', password: 'anup' });
-        cookie = loginResponse.get('Set-Cookie')
-    })
+        const loginResponse = await request(app)
+            .post('/api/login')
+            .send({ email: 'anup.poudel@ambine.com', password: 'anup' });
+        cookie = loginResponse.get('Set-Cookie');
+    });
 
     afterAll(async () => {
-        await db.dropCollection('products')
-    })
+        await db.dropCollection('products');
+    });
 
     it('should return all products', async () => {
         const res = await request(app).get('/api/products');
@@ -28,10 +30,10 @@ describe('GET /api/products', () => {
     });
 
     it('should return filtered results if query parameters are present', async () => {
-        const queryParam = 'title'
-        const queryValue = 'nike'
-        const regex = new RegExp(queryValue, "i")
-        const count = await db.collections.products.countDocuments({ [queryParam]: { $regex: regex } })
+        const queryParam = 'title';
+        const queryValue = 'nike';
+        const regex = new RegExp(queryValue, 'i');
+        const count = await db.collections.products.countDocuments({ [queryParam]: { $regex: regex } });
         const res = await request(app).get(`/api/products?${queryParam}=${queryValue}`);
         expect(res.status).toBe(200);
         expect(res.headers['content-type']).toEqual(expect.stringContaining('json'));
@@ -39,10 +41,31 @@ describe('GET /api/products', () => {
     });
 
     it('should create a product', async () => {
-        const response = await request(app).post('/api/products').set("Cookie", cookie).send(testProduct);
+        const response = await request(app).post('/api/products').set('Cookie', cookie).send(testProduct);
         expect(response.status).toBe(200);
         const createdProduct = await db.collections.products.findOne({ ...testProduct });
         expect(createdProduct).toBeTruthy();
-    })
+    });
 
+    it('should find a product', async () => {
+        const product = await db.collections.products.findOne();
+        if (product) {
+            const response = await request(app).get(`/api/products/${product._id.toString()}`);
+            expect(response.status).toBe(200);
+            expect(response.body.data).toEqual({ ...product, _id: product._id.toString() });
+        } else {
+            throw new Error('Test data for product not found');
+        }
+    });
+
+    it('should delete a product', async () => {
+        const product = await db.collections.products.findOne();
+        if (product) {
+            const response = await request(app).delete(`/api/products/${product._id.toString()}`).set('Cookie', cookie);
+            expect(response.status).toBe(200);
+            const foundProduct = await db.collections.products.findOne({ _id: product._id });
+            expect(response.body.data).toEqual({ ...product, _id: product._id.toString() });
+            expect(foundProduct).toBeFalsy();
+        }
+    });
 });
