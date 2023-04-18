461c717ddf5c7c6a12bb4cf169e34def86a97e08
anup-an <friek_an@yahoo.com>
2023-04-19 00:57:36 +0300

    Add types

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -7,15 +7,15 @@ import { IUser } from '../schemas/user';
 import { GenericService } from '../services/common';
 
 const router = Router();
-const userService = new GenericService<IUser>(User)
+const userService = new GenericService<IUser>(User);
 const userController = new GenericController<IUser>(userService);
 
 router.get(
     '/',
     (req, res, next) => {
-        passport.authenticate('jwt', { session: false }, (err, user) => {
+        passport.authenticate('jwt', { session: false }, (err: any, user: IUser | undefined) => {
             if (user) {
-                req.params.id = (user as IUser)?._id.toString();
+                req.params['id'] = user._id.toString();
                 next();
             } else {
                 res.status(200).json({ data: guestUser });
@@ -27,9 +27,9 @@ router.get(
 router.post(
     '/',
     (req, res, next) => {
-        passport.authenticate('jwt', { session: false }, (err, user) => {
+        passport.authenticate('jwt', { session: false }, (err: any, user: IUser | undefined) => {
             if (user) {
-                req.params.id = (user as IUser)?._id.toString();
+                req.params['id'] = user._id.toString();
                 next();
             } else {
                 res.status(401).send('Unauthorized');
