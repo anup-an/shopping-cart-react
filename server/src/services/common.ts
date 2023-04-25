9bef93d9894565f075a7b4b822cee67ed93805a6
anup-an <friek_an@yahoo.com>
2023-04-25 12:02:19 +0300

    Handle requests related to sort, search and filter in the same generic handler and service

--- server/src/services/common.ts
+++ server/src/services/common.ts
@@ -2,16 +2,36 @@ import { Request, Response } from 'express';
 import _ from 'lodash';
 
 import { IModel } from '../models/model';
+import { FilterQuery } from 'mongoose';
+
+const queryStringParser = (queryString?: string): { [key: string]: any } => {
+    const arrayAfterFormat =
+        queryString?.split(';').map((val) => {
+            const splitString = val.split(':');
+            return splitString[0] && splitString[1] ? { [splitString[0]]: splitString[1] } : {};
+        }) || [];
+
+    return arrayAfterFormat.reduce((acc, val) => {
+        return { ...acc, ...val };
+    }, {});
+};
 
 export class GenericService<T> {
     constructor(private readonly model: IModel<T>) {}
 
     async getAll(req: Request, res: Response) {
-        return await this.model.find(
-            _.mapValues(req.query, (val) => {
-                return { $regex: val, $options: 'i' };
-            }),
-        );
+        const searchObject = queryStringParser(req.query.search as string | undefined);
+        const search = _.keys(searchObject).map((val) => ({
+            [val]: { $regex: searchObject[val], $options: 'i' },
+        }));
+        const sort = queryStringParser(req.query.sort as string | undefined);
+        const filter = queryStringParser(req.query.filter as string | undefined);
+        return await this.model
+            .find({
+                $or: _.isEmpty(search) ? [{}] : search,
+                ...filter,
+            } as FilterQuery<T>)
+            .sort({ ...sort });
     }
 
     async getById(req: Request, res: Response) {
