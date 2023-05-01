e2b67b47a9c86680a17b092f51de5865b6f60a3d
anup-an <friek_an@yahoo.com>
2023-05-02 02:53:11 +0300

    Add handlers and routes for password reset feature

--- server/src/controllers/authentication.controller.ts
+++ server/src/controllers/authentication.controller.ts
@@ -1,11 +1,14 @@
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import _ from 'lodash';
-import mongoose from 'mongoose';
 
+import Token from '../models/token';
 import User from '../models/user';
 import { IUser } from '../schemas/user';
 import { ErrorCode, ErrorException } from '../utils';
+import sendEmail from '../utils/sendEmail';
 
 export type VerifiedUser = IUser & Express.User;
 
@@ -90,6 +93,56 @@ export const reIssueTokens = async (req: Request, res: Response, next: NextFunct
         } else {
             throw new ErrorException(ErrorCode.AuthenticationError, 'Unauthorized');
         }
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const sendPasswordResetLink = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) throw new ErrorException(ErrorCode.ResetPasswordError, 'User does not exist');
+
+        const token = await Token.findOne({ userId: user._id });
+        if (token) await token.deleteOne();
+        const resetToken = crypto.randomBytes(32).toString('hex');
+        const tokenHash = await bcrypt.hash(resetToken, 10);
+
+        await new Token({
+            userId: user._id,
+            token: tokenHash,
+            createdAt: Date.now(),
+        }).save();
+
+        await sendEmail(
+            email,
+            'Reset your password',
+            `You have asked to reset your password. Open then link, ${
+                process.env.HOST || 'http://localhost:3000'
+            }/reset-password?token=${tokenHash} and follow the instructions. The link can be used only once`,
+        );
+        res.status(200).send('Password reset link sent');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const resetPassword = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { password, resetToken } = req.body;
+        const result = await Token.findOne({ token: resetToken });
+        if (!result) throw new ErrorException(ErrorCode.ResetPasswordError, 'Invalid or expired password reset token');
+
+        const user = await User.findOneAndUpdate(
+            { _id: result.toObject().userId },
+            { password: await bcrypt.hash(password, 10) },
+            {new: true},
+        );
+        if (!user) throw new ErrorException(ErrorCode.ResetPasswordError, 'User not found');
+
+        await Token.findOneAndDelete({ userId: user._id });
+        res.status(200).send('Password reset successful');
     } catch (error) {
         next(error);
     }
--- server/src/routes/authentication.routes.ts
+++ server/src/routes/authentication.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import passport from 'passport';
-import { signupUser, loginUser, logoutUser, reIssueTokens } from '../controllers/authentication.controller';
+import { signupUser, loginUser, logoutUser, reIssueTokens, sendPasswordResetLink, resetPassword } from '../controllers/authentication.controller';
 import { verifyLoginCredentials, verifyRefreshToken } from '../middlewares/authenticate';
 
 const router = Router();
 
 router.post('/signup', signupUser);
+router.post('/reset-link', sendPasswordResetLink);
+router.post('/reset-password', resetPassword);
 router.post('/login', verifyLoginCredentials, loginUser);
 router.post('/logout', passport.authenticate('jwt', { session: false }), logoutUser);
 router.post('/reissue-token', passport.authenticate('jwt', { session: false }), verifyRefreshToken, reIssueTokens);
