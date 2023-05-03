20a64b63e361033e98f7d97d932409e055498bac
anup-an <friek_an@yahoo.com>
2023-05-03 14:34:07 +0300

    Add generic functions which can be used to call appropriate functions on success or failure of api requests

--- src/types/mapDataTypes.ts
+++ src/types/mapDataTypes.ts
@@ -26,6 +26,26 @@ export const isLoading = <T, E>(mapData: MapData<T, E>) => mapData.status === 'L
 export const isSuccess = <T, E>(mapData: MapData<T, E>) => mapData.status === 'Success';
 export const isFailure = <T, E>(mapData: MapData<T, E>) => mapData.status === 'Failure';
 
-export const extractData = <T, E, O>(mapData: MapData<T, E>, prop: keyof T, defaultValue: O): O => {
+export const pickFieldOrDefault = <T, E, O>(mapData: MapData<T, E>, prop: keyof T, defaultValue: O): O => {
     return isSuccess(mapData) ? (_.get((mapData as Success<T>).data, prop) as O) : defaultValue;
+};
+
+export const extractDataOrNull = <T, E>(mapData: MapData<T, E>): T | null =>
+    isSuccess(mapData) ? (mapData as Success<T>).data : null;
+
+export const extractError = <T, E>(mapData: MapData<T, E>) =>
+    isFailure(mapData) ? _.get(mapData as Failure<E>, 'error') : null;
+
+export const useMapData = <T, E>(
+    mapData: MapData<T, E>,
+    onSuccess: (value: T) => void,
+    onError: (value: E) => void,
+): void => {
+    if (isSuccess(mapData)) {
+        const data = extractDataOrNull<T, E>(mapData);
+        if (data !== null) onSuccess(data);
+    } else {
+        const error = extractError(mapData);
+        if (error !== null) onError(error);
+    }
 };
