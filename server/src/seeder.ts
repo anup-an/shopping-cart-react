19505bc4e795ed1eb170d759a7937423d34e6a2d
anup-an <friek_an@yahoo.com>
2023-05-09 09:37:00 +0300

    Add a script to seed the products data to the database

---
+++ server/src/seeder.ts
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+
+import { IModel } from './models/model';
+import Product from './models/product';
+import { ProductSeed, products } from '../tests/testData';
+import { IProduct } from './schemas/product';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const seedDatabase = async <T, K>(model: IModel<T>, data: K[]): Promise<void> => {
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URI|| '')
+        await model.deleteMany({})
+        const seed = await model.insertMany(data);
+        console.log(`Seeding complete. ${seed.length} out of ${data.length} ${model.collection.collectionName} added to the database`);
+        db.disconnect()
+    } catch (error) {
+        console.log('Failed to seed the database')
+    }
+};
+
+seedDatabase<IProduct, ProductSeed>(Product, products);
--- server/tests/testData.ts
+++ server/tests/testData.ts
@@ -1,154 +1,142 @@
-export const products = [
-    {
-      "id": "1",
-      "title": "Nike Casual for Men",
-      "image": "/images/shoe1-min.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "price": 25,
-      "availableSizes": [
-        "XL",
-        "XXL"
-      ]
-    },
-    {
-      "id": "2",
-      "title": "Nike Casual for Women ",
-      "image": "/images/shoe2-min.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "price": 20,
-      "availableSizes": [
-        "M",
-        "XL",
-        "XXL"
-      ]
-    },
-    {
-      "id": "3",
-      "title": "Addidas Casual Multicolor",
-      "image": "/images/shoe3-min.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "price": 30,
-      "availableSizes": [
-        "S",
-        "M",
-        "XL",
-        "XXL"
-      ]
-    },
-    {
-      "id": "4",
-      "title": "AllStar Converse Shoe",
-      "image": "/images/shoe4-min.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "price": 22,
-      "availableSizes": [
-        "L",
-        "XL",
-        "XXL"
-      ]
-    },
-    {
-      "id": "5",
-      "title": "Nike Trainer Shoe Red",
-      "image": "/images/shoe5-min.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "price": 21,
-      "availableSizes": [
-        "XXL"
-      ]
-    },
-    {
-      "id": "6",
-      "title": "Converse Shoe White",
-      "image": "/images/shoe6-min.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "price": 25,
-      "availableSizes": [
-        "XL",
-        "XXL"
-      ]
-    },
-    {
-      "id": "7",
-      "title": "Leather Shoe for men",
-      "image": "/images/shoe7-min.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "price": 40,
-      "availableSizes": [
-        "XL"
-      ]
-    },
-    {
-      "id": "8",
-      "title": "Nike Shoe White for men",
-      "image": "/images/shoe8-min.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "price": 25,
-      "availableSizes": [
-        "S"
-      ]
-    },
-    {
-      "id": "9",
-      "title": "Nike Trainer Shoe Blue",
-      "image": "/images/shoe9-min.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "price": 40,
-      "availableSizes": [
-        "L",
-        "XL",
-        "XXL"
-      ]
-    },
-    {
-      "id": "10",
-      "title": "Nike Casual Shoe Red",
-      "image": "/images/shoe10-min.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "price": 30,
-      "availableSizes": [
-        "L",
-        "XL"
-      ]
-    }
-]
-
-export const testProduct = {
-    "id": "123",
-    "title": "Test product",
-    "image": "/images/shoe1-min.jpg",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "price": 35,
-    "availableSizes": [
-        "XL",
-        "XXL"
-    ]
+export interface ProductSeed {
+    id: string;
+    title: string;
+    image: string;
+    description: string;
+    price: number;
+    availableSizes: string[];
 }
 
-export const testCart = [{
-  ...testProduct, count: 2
-}]
-        
+export const products: ProductSeed[] = [
+    {
+        id: '1',
+        title: 'Nike Casual for Men',
+        image: '/images/shoe1-min.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        price: 25,
+        availableSizes: ['XL', 'XXL'],
+    },
+    {
+        id: '2',
+        title: 'Nike Casual for Women ',
+        image: '/images/shoe2-min.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        price: 20,
+        availableSizes: ['M', 'XL', 'XXL'],
+    },
+    {
+        id: '3',
+        title: 'Addidas Casual Multicolor',
+        image: '/images/shoe3-min.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        price: 30,
+        availableSizes: ['S', 'M', 'XL', 'XXL'],
+    },
+    {
+        id: '4',
+        title: 'AllStar Converse Shoe',
+        image: '/images/shoe4-min.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        price: 22,
+        availableSizes: ['L', 'XL', 'XXL'],
+    },
+    {
+        id: '5',
+        title: 'Nike Trainer Shoe Red',
+        image: '/images/shoe5-min.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        price: 21,
+        availableSizes: ['XXL'],
+    },
+    {
+        id: '6',
+        title: 'Converse Shoe White',
+        image: '/images/shoe6-min.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        price: 25,
+        availableSizes: ['XL', 'XXL'],
+    },
+    {
+        id: '7',
+        title: 'Leather Shoe for men',
+        image: '/images/shoe7-min.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        price: 40,
+        availableSizes: ['XL'],
+    },
+    {
+        id: '8',
+        title: 'Nike Shoe White for men',
+        image: '/images/shoe8-min.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        price: 25,
+        availableSizes: ['S'],
+    },
+    {
+        id: '9',
+        title: 'Nike Trainer Shoe Blue',
+        image: '/images/shoe9-min.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        price: 40,
+        availableSizes: ['L', 'XL', 'XXL'],
+    },
+    {
+        id: '10',
+        title: 'Nike Casual Shoe Red',
+        image: '/images/shoe10-min.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        price: 30,
+        availableSizes: ['L', 'XL'],
+    },
+];
+
+export const testProduct = {
+    id: '123',
+    title: 'Test product',
+    image: '/images/shoe1-min.jpg',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: 35,
+    availableSizes: ['XL', 'XXL'],
+};
+
+export const testCart = [
+    {
+        ...testProduct,
+        count: 2,
+    },
+];
+
 export const registerPayload = {
     email: 'anup.poudel@ambine.com',
     password: 'Testing1#',
     firstName: 'Anup',
     lastName: 'Poudel',
-}
+};
 
 export const invalidRegisterPayload = {
-  email: 'anup.poudel.com',
+    email: 'anup.poudel.com',
     password: 'Testing',
     firstName: 'Anup',
     lastName: 'Poudel',
-}
+};
 
 export const testOrderPayload = {
-  name: 'Anup Poudel',
-  email: 'anup.poudel@ambine.com',
-  address: 'Test address',
-  postcode: 20610,
-  city: 'Turku',
-  country: 'Finland',
-  cart: testCart
-}
+    name: 'Anup Poudel',
+    email: 'anup.poudel@ambine.com',
+    address: 'Test address',
+    postcode: 20610,
+    city: 'Turku',
+    country: 'Finland',
+    cart: testCart,
+};
