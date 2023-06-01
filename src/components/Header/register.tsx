593849608e2e6246a7f5494ba94e9e5ee72e086c
anup-an <friek_an@yahoo.com>
2023-06-01 15:55:30 +0300

    Update login and register layout in small screen

--- src/components/Header/register.tsx
+++ src/components/Header/register.tsx
@@ -74,7 +74,7 @@ class Register extends React.Component<IProps, IState> {
         const { user } = this.props;
 
         return (
-            <div className="flex flex-row flex items-center justify-center w-full h-full pt-14">
+            <div className="flex flex-row flex items-center justify-center w-full h-full lg:pt-14">
                 {!user?._id ? (
                     <div className="bg-white border rounded-lg shadow-xl w-full lg:w-2/3 flex flex-row items-center lg:bg-blue-800">
                         <div className="hidden lg:block w-1/2 text-white p-4 flex items-center justify-center text-center flex-col">
