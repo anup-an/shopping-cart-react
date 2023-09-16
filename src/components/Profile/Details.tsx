47ea00b485ac34723f26be33a77752282a62a83f
anup-an <friek_an@yahoo.com>
2023-09-17 01:27:52 +0300

    Fix profile page layout issues

--- src/components/Profile/Details.tsx
+++ src/components/Profile/Details.tsx
@@ -7,127 +7,127 @@ import { editUserProfile } from '../../actions/userAction';
 import { AppState } from '../../store';
 
 interface IProps {
-    user: IUser;
-    actions: Actions;
+  user: IUser;
+  actions: Actions;
 }
 
 interface IState {
-    firstName: string;
-    lastName: string;
-    phone: string;
-    email: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
 }
 
 interface Actions {
-    editUserProfile: (userData: Partial<IUser>) => void;
+  editUserProfile: (userData: Partial<IUser>) => void;
 }
 
 class Details extends React.Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-        this.state = {
-            firstName: this.props.user.firstName,
-            lastName: this.props.user.lastName,
-            phone: this.props.user.phone,
-            email: this.props.user.email,
-        };
-    }
-    handleSave = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        this.props.actions.editUserProfile({ ...this.state });
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      firstName: this.props.user.firstName,
+      lastName: this.props.user.lastName,
+      phone: this.props.user.phone,
+      email: this.props.user.email,
     };
+  }
+  handleSave = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.actions.editUserProfile({ ...this.state });
+  };
 
-    handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({ ...this.state, [event.target.name]: event.target.value });
-    };
+  handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ ...this.state, [event.target.name]: event.target.value });
+  };
 
-    componentDidUpdate(prevProps: IProps): void {
-        if (
-            !_.isEqual(prevProps, this.props) &&
-            !_.isEqual(_.pick(this.props.user, ['firstName', 'lastName', 'phone', 'email']), this.state)
-        ) {
-            this.setState({
-                ...this.state,
-                firstName: this.props.user.firstName,
-                lastName: this.props.user.lastName,
-                phone: this.props.user.phone,
-                email: this.props.user.email,
-            });
-        }
+  componentDidUpdate(prevProps: IProps): void {
+    if (
+      !_.isEqual(prevProps, this.props) &&
+      !_.isEqual(_.pick(this.props.user, ['firstName', 'lastName', 'phone', 'email']), this.state)
+    ) {
+      this.setState({
+        ...this.state,
+        firstName: this.props.user.firstName,
+        lastName: this.props.user.lastName,
+        phone: this.props.user.phone,
+        email: this.props.user.email,
+      });
     }
-    render() {
-        const { firstName, lastName, phone, email } = this.props.user;
-        return (
-            <div className="border rounded shadow-xl w-full h-full p-4 flex flex-col space-y-10">
-                <p className="text-xl lg:text-2xl">My details</p>
-                <form className="flex flex-col space-y-6" onSubmit={this.handleSave}>
-                    <p className="py-2 border-b w-full">Personal information</p>
-                    <div className="flex flex-col space-y-4 lg:flex-row items-start lg:space-x-10">
-                        <div>Edit the fields and click save to update your details.</div>
-                        <div className="flex flex-col space-y-6">
-                            <div className="flex flex-row space-x-12">
-                                <div>
-                                    <p>First Name</p>
-                                    <label htmlFor="firstName">
-                                        <input
-                                            name="firstName"
-                                            id="firstName"
-                                            className="p-2 bg-gray-200 border rounded"
-                                            defaultValue={firstName}
-                                            onChange={this.handleInput}
-                                            required
-                                        />
-                                    </label>
-                                </div>
-                                <div>
-                                    <p>Last Name</p>
-                                    <label htmlFor="lastName">
-                                        <input
-                                            name="lastName"
-                                            id="lastName"
-                                            className="p-2 bg-gray-200 border rounded"
-                                            defaultValue={lastName}
-                                            onChange={this.handleInput}
-                                            required
-                                        />
-                                    </label>
-                                </div>
-                            </div>
-                            <div>
-                                <p>Phone number</p>
-                                <label htmlFor="phone">
-                                    <input
-                                        name="phone"
-                                        id="phone"
-                                        className="p-2 bg-gray-200 border rounded"
-                                        defaultValue={phone}
-                                        onChange={this.handleInput}
-                                    />
-                                </label>
-                            </div>
-                            <button className="w-1/4 border rounded focus:outline-none p-2 bg-blue-400 hover:bg-blue-800 text-white">
-                                Save
-                            </button>
-                        </div>
-                    </div>
-                </form>
+  }
+  render() {
+    const { firstName, lastName, phone, email } = this.props.user;
+    return (
+      <div className="border rounded shadow-xl w-full h-full p-4 flex flex-col space-y-10">
+        <p className="text-xl lg:text-2xl">My details</p>
+        <form className="flex flex-col space-y-6" onSubmit={this.handleSave}>
+          <p className="py-2 border-b w-full">Personal information</p>
+          <div className="flex flex-col space-y-4 lg:flex-row items-start lg:space-x-10">
+            <div>Edit the fields and click save to update your details.</div>
+            <div className="flex flex-col space-y-6">
+              <div className="flex flex-col md:flex-row space-y-6 md:space-y-0">
+                <div>
+                  <p>First Name</p>
+                  <label htmlFor="firstName" className="md:mr-12">
+                    <input
+                      name="firstName"
+                      id="firstName"
+                      className="p-2 bg-gray-200 border rounded"
+                      defaultValue={firstName}
+                      onChange={this.handleInput}
+                      required
+                    />
+                  </label>
+                </div>
+                <div>
+                  <p>Last Name</p>
+                  <label htmlFor="lastName">
+                    <input
+                      name="lastName"
+                      id="lastName"
+                      className="p-2 bg-gray-200 border rounded"
+                      defaultValue={lastName}
+                      onChange={this.handleInput}
+                      required
+                    />
+                  </label>
+                </div>
+              </div>
+              <div>
+                <p>Phone number</p>
+                <label htmlFor="phone">
+                  <input
+                    name="phone"
+                    id="phone"
+                    className="p-2 bg-gray-200 border rounded"
+                    defaultValue={phone}
+                    onChange={this.handleInput}
+                  />
+                </label>
+              </div>
+              <button className="w-1/4 border rounded focus:outline-none p-2 bg-blue-400 hover:bg-blue-800 text-white">
+                Save
+              </button>
             </div>
-        );
-    }
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 interface StateProps {
-    user: IUser;
+  user: IUser;
 }
 
 const mapStateToProps = (state: AppState): StateProps => ({
-    user: state.user.user,
+  user: state.user.user,
 });
 
 const mapDispatchToProps = (dispatch: any): { actions: Actions } => ({
-    actions: {
-        editUserProfile: (userData: Partial<IUser>) => dispatch(editUserProfile(userData)),
-    },
+  actions: {
+    editUserProfile: (userData: Partial<IUser>) => dispatch(editUserProfile(userData)),
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Details);
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -6,96 +6,91 @@ import { IUser } from '../../ActionTypes';
 type IProps = RouteComponentProps<{}, {}, IUser>;
 
 interface IComponentProps extends IProps {
-    focus: string;
+  focus: string;
 }
 
 class Profile extends React.Component<IComponentProps> {
-    render() {
-        const user = this.props.location.state;
-        const { focus } = this.props;
-        return (
-            <div className="w-full flex flex-row lg:flex-col lg:space-y-10">
-                <div className="hidden lg:block text-2xl lg:text-3xl p-2 mt-10">My Account</div>
-                <div className="w-full flex flex-row justify-between lg:flex-col lg:space-y-6">
-                    <button
-                        type="button"
-                        onClick={() => this.props.history.push('/user/profile')}
-                        className={`flex flex-row items-center space-x-4 hover:bg-gray-300 ${
-                            focus === 'details' ? 'bg-blue-400 hover:bg-blue-800 text-white' : ''
-                        } focus:outline-none border-0 rounded px-2 py-1`}
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                            />
-                        </svg>
-                        <p>My details</p>
-                    </button>
-                    <button
-                        type="button"
-                        onClick={() => this.props.history.push('/user/address')}
-                        className={`flex flex-row items-center space-x-4 hover:bg-gray-300 ${
-                            focus === 'address' ? 'bg-blue-400 hover:bg-blue-800 text-white' : ''
-                        } focus:outline-none border-0 rounded px-2 py-1`}
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                            />
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                            />
-                        </svg>
-                        <p>My Address</p>
-                    </button>
-                    <button
-                        type="button"
-                        onClick={() => this.props.history.push('/user/orders')}
-                        className={`flex flex-row items-center space-x-4 hover:bg-gray-300 ${
-                            focus === 'orders' ? 'bg-blue-400 hover:bg-blue-800 text-white' : ''
-                        } focus:outline-none border-0 rounded px-2 py-1`}
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                            />
-                        </svg>
-                        <p>My orders</p>
-                    </button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const user = this.props.location.state;
+    const { focus } = this.props;
+    return (
+      <div className="w-full bg-white fixed md:relative flex flex-row lg:flex-col lg:space-y-10">
+        <div className="hidden lg:block text-2xl lg:text-3xl p-2 mt-10">My Account</div>
+        <div className="w-full flex flex-col md:flex-row justify-between lg:flex-col lg:space-y-6">
+          <button
+            type="button"
+            onClick={() => this.props.history.push('/user/profile')}
+            className={`flex flex-row items-center space-x-4 hover:bg-gray-300 ${
+              focus === 'details' ? 'bg-blue-400 hover:bg-blue-800 text-white' : ''
+            } focus:outline-none border-0 rounded px-2 py-1`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <p>My details</p>
+          </button>
+          <button
+            type="button"
+            onClick={() => this.props.history.push('/user/address')}
+            className={`flex flex-row items-center space-x-4 hover:bg-gray-300 ${
+              focus === 'address' ? 'bg-blue-400 hover:bg-blue-800 text-white' : ''
+            } focus:outline-none border-0 rounded px-2 py-1`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+              />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            <p>My Address</p>
+          </button>
+          <button
+            type="button"
+            onClick={() => this.props.history.push('/user/orders')}
+            className={`flex flex-row items-center space-x-4 hover:bg-gray-300 ${
+              focus === 'orders' ? 'bg-blue-400 hover:bg-blue-800 text-white' : ''
+            } focus:outline-none border-0 rounded px-2 py-1`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+              />
+            </svg>
+            <p>My orders</p>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Profile);
