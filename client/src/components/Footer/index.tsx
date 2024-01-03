feb2bcdcdf4f0cd1ed96886c92a6a40ced2fedb4
anup-an <friek_an@yahoo.com>
2024-01-03 04:29:12 +0200

    Update navigation bar

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,118 +1,110 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link, RouteComponentProps } from 'react-router-dom';
-import { withRouter } from "react-router";
+import { withRouter } from 'react-router';
 import { logOutUser } from '../../actions/userAction';
 import { IUser } from '../../ActionTypes';
 import { AppState } from '../../store';
 
 interface IProps extends RouteComponentProps {
-    user: IUser;
-    actions: Actions;
-    focus: string;
+  user: IUser;
+  actions: Actions;
+  focus: string;
 }
 
 interface Actions {
-    logOutUser: () => Promise<void>;
+  logOutUser: () => Promise<void>;
 }
 
 class Footer extends React.Component<IProps> {
-    handleLogOut = () => {
-        const { user } = this.props;
-        this.props.actions.logOutUser();
-    }
-    render() {
-        const { user } = this.props;
-        return (
-            <div className="bg-blue-800 p-2 text-sm text-white flex flex-row justify-between items-center">
-                <div className="flex flex-row justify-between items-center gap-x-10">
-                    {user._id == "" ?
-                        <Link to="/login" className={`border hover:border-white ${this.props.focus === "login" ? "border-white": "border-blue-800"} flex flex-row items-center p-1`}>
-                            <p>Login</p>
-                            <svg
-                                className="text-white w-6 h-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none" stroke="currentColor"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                                />
-                            </svg>
-                        </Link> :
-                        <div className="flex flex-row items-center space-x-4">
-                            <p className="text text-sm">Welcome {user.firstName}!</p>
-                            <button
-                                type="button"
-                                onClick={() => this.props.history.push({
-                                    pathname: "/user/profile",
-                                    state: user,
-                                })}
-                                className={`border hover:border-white ${this.props.focus==="profile" ? "lg:border-white" : "lg:border-blue-800"} focus:outline-none p-2`}
-                            >
-                            My account
-                            </button>
-                            </div>
-                    }
-                    {user._id == "" ?
-                        <Link to="/register" className={`flex flex-row items-center p-1 border hover:border-white ${this.props.focus === "register" ? "border-white": "border-blue-800"}`}>
-                            <p>Register</p>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24" height="24" fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                className="feather feather-file-text"
-                            >
-                                <path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8z" />
-                                <path d="M14 2v6h6M16 13H8M16 17H8M10 9H8" />
-                            </svg>
-                        </Link> :
-                        <button
-                            onClick={this.handleLogOut}
-                            type="button"
-                            className="flex flex-row items-center border border-blue-800 focus:border-white p-2"
-                        >
-                            <p>Logout</p>
-                            <svg
-                                className="w-6 h-6 text-white"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none" stroke="currentColor"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                                />
-                            </svg>
-                        </button>
-                    }
-
-                </div>
-
+  handleLogOut = () => {
+    const { user } = this.props;
+    this.props.actions.logOutUser();
+  };
+  render() {
+    const { user } = this.props;
+    return (
+      <div className="bg-blue-800 p-3 text-sm text-white flex flex-row justify-between items-center">
+        <div className="flex flex-row justify-between items-center gap-x-10">
+          {user._id == '' ? (
+            <Link
+              to="/login"
+              className={`border hover:border-white ${
+                this.props.focus === 'login' ? 'border-white' : 'border-blue-800'
+              } flex flex-row items-center p-1`}
+            >
+              <p>Login</p>
+              <i className="fa fa-sign-in ml-1" aria-hidden="true"></i>
+            </Link>
+          ) : (
+            <div className="flex flex-row items-center space-x-4">
+              <p className="text text-sm">Welcome {user.firstName}!</p>
+              <button
+                type="button"
+                onClick={() =>
+                  this.props.history.push({
+                    pathname: '/user/profile',
+                    state: user,
+                  })
+                }
+                className={`border hover:border-white ${
+                  this.props.focus === 'profile' ? 'lg:border-white' : 'lg:border-blue-800'
+                } focus:outline-none p-2`}
+              >
+                My account
+              </button>
             </div>
-        )
-    }
+          )}
+          {user._id == '' ? (
+            <Link
+              to="/register"
+              className={`flex flex-row items-center p-1 border hover:border-white ${
+                this.props.focus === 'register' ? 'border-white' : 'border-blue-800'
+              }`}
+            >
+              <p>Register</p>
+              <i className="fa fa-user-plus"></i>
+            </Link>
+          ) : (
+            <button
+              onClick={this.handleLogOut}
+              type="button"
+              className="flex flex-row items-center border border-blue-800 focus:border-white p-2"
+            >
+              <p>Logout</p>
+              <svg
+                className="w-6 h-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                />
+              </svg>
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 interface StateProps {
-    user: IUser;
+  user: IUser;
 }
 const mapStateToProps = (state: AppState): StateProps => ({
-    user: state.user.user,
-})
+  user: state.user.user,
+});
 
 const mapDispatchToProps = (dispatch: any) => ({
-    actions: {
-        logOutUser: () => dispatch(logOutUser()),
-    }
-})
+  actions: {
+    logOutUser: () => dispatch(logOutUser()),
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Footer));
--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -9,155 +9,132 @@ import { AppState } from '../../store';
 import { SearchState } from '../../types/common';
 
 type Actions = {
-    logOutUser: () => Promise<void>;
+  logOutUser: () => Promise<void>;
 };
 
 interface IProps extends RouteComponentProps {
-    actions: Actions;
-    user: IUser;
-    focus: string;
+  actions: Actions;
+  user: IUser;
+  focus: string;
 }
 
 type IState = {
-    isOpen: boolean;
+  isOpen: boolean;
 };
 
 class Header extends React.Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-        this.state = { isOpen: false };
-    }
-    openModal = () => {
-        this.setState((state) => ({ ...state, isOpen: true }));
-    };
+  constructor(props: IProps) {
+    super(props);
+    this.state = { isOpen: false };
+  }
+  openModal = () => {
+    this.setState((state) => ({ ...state, isOpen: true }));
+  };
 
-    closeModal = () => {
-        this.setState((state) => ({ ...state, isOpen: false }));
-    };
+  closeModal = () => {
+    this.setState((state) => ({ ...state, isOpen: false }));
+  };
 
-    handleLogOut = () => {
-        const { user } = this.props;
-        this.props.actions.logOutUser();
-        this.props.history.push('/');
-    };
-    render() {
-        const { user, focus } = this.props;
-        return (
-            <div className={`flex flex-row bg-blue-800 text-white text-xl justify-between p-4 items-center`}>
-                <Link
-                    to="/"
-                    className={`block border hover:border-white lg:hidden p-2 ${
-                        focus === 'home' ? 'border-white' : 'border-blue-800'
-                    }`}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        className="w-6 h-6 text-white"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                        />
-                    </svg>
-                </Link>
-                <Link
-                    to="/"
-                    className={`hidden lg:block border lg:hover:border-white ${
-                        focus === 'home' ? 'lg:border-white' : 'border-blue-800'
-                    } p-2`}
-                >
-                    Home
-                </Link>
-                <div className="flex flex-row justify-between items-center gap-x-10">
-                    {!user._id ? (
-                        <Link
-                            to="/login"
-                            className={`hidden lg:block border lg:hover:border-white ${
-                                focus === 'login' ? 'lg:border-white' : 'lg:border-blue-800'
-                            } focus:outline-none p-2`}
-                        >
-                            Login
-                        </Link>
-                    ) : (
-                        <div className="flex flex-row items-center space-x-4">
-                            <p className="hidden lg:block text text-sm">Welcome {user.firstName}!</p>
-                            <button
-                                type="button"
-                                onClick={() =>
-                                    this.props.history.push({
-                                        pathname: '/user/profile',
-                                        state: user,
-                                    })
-                                }
-                                className={`hidden lg:block lg:hover:border-white border ${
-                                    focus === 'profile' ? 'lg:border-white' : 'lg:border-blue-800'
-                                } focus:outline-none p-2`}
-                            >
-                                My account
-                            </button>
-                        </div>
-                    )}
-                    {!user._id ? (
-                        <Link
-                            to="/register"
-                            className={`hidden lg:block lg:hover:border-white border ${
-                                focus === 'register' ? 'lg:border-white' : 'lg:border-blue-800'
-                            } p-2`}
-                        >
-                            Register
-                        </Link>
-                    ) : (
-                        <button
-                            onClick={this.handleLogOut}
-                            type="button"
-                            className="hidden lg:block lg:hover:border-white border border-blue-800 p-2"
-                        >
-                            Logout
-                        </button>
-                    )}
-                    <Link
-                        to="/cart"
-                        className={`border hover:border-white ${
-                            focus === 'cart' ? 'border-white' : 'border-blue-800'
-                        } p-2`}
-                    >
-                        <svg
-                            className="focus:outline-none h-6 w-6 text-white"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                            />
-                        </svg>
-                    </Link>
-                </div>
+  handleLogOut = () => {
+    const { user } = this.props;
+    this.props.actions.logOutUser();
+    this.props.history.push('/');
+  };
+  render() {
+    const { user, focus } = this.props;
+    return (
+      <div className={`flex flex-row bg-blue-800 text-white text-md justify-between p-3 items-center`}>
+        <Link
+          to="/"
+          className={`p-1 border hover:border-white ${focus === 'home' ? 'border-white' : 'border-blue-800'}`}
+        >
+          <img src="/logo-shoe-eshop.jpg" className="h-6" />
+        </Link>
+        <div className="flex flex-row justify-between items-center gap-x-14">
+          {!user._id ? (
+            <Link
+              to="/login"
+              className={`hidden lg:flex border hover:border-white ${
+                this.props.focus === 'login' ? 'border-white' : 'border-blue-800'
+              } flex-row items-center p-1`}
+            >
+              <p>Login</p>
+              <i className="fa fa-sign-in ml-1" aria-hidden="true"></i>
+            </Link>
+          ) : (
+            <div className="flex flex-row items-center space-x-6">
+              <p className="hidden lg:block text text-sm">Welcome {user.firstName}!</p>
+              <button
+                type="button"
+                onClick={() =>
+                  this.props.history.push({
+                    pathname: '/user/profile',
+                    state: user,
+                  })
+                }
+                className={`hidden lg:block lg:hover:border-white border ${
+                  focus === 'profile' ? 'lg:border-white' : 'lg:border-blue-800'
+                } focus:outline-none p-2`}
+              >
+                My account
+              </button>
             </div>
-        );
-    }
+          )}
+          {!user._id ? (
+            <Link
+              to="/register"
+              className={`hidden lg:flex flex-row items-center p-1 border hover:border-white ${
+                this.props.focus === 'register' ? 'border-white' : 'border-blue-800'
+              }`}
+            >
+              <p>Register</p>
+              <i className="fa fa-user-plus ml-1"></i>
+            </Link>
+          ) : (
+            <button
+              onClick={this.handleLogOut}
+              type="button"
+              className="hidden lg:block lg:hover:border-white border border-blue-800 p-2"
+            >
+              Logout
+            </button>
+          )}
+          <Link
+            to="/cart"
+            className={`border hover:border-white ${focus === 'cart' ? 'border-white' : 'border-blue-800'} p-1`}
+          >
+            <svg
+              className="focus:outline-none h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 interface StateProps {
-    user: IUser;
+  user: IUser;
 }
 
 const mapStateToProps = (state: AppState): StateProps => ({
-    user: state.user.user,
+  user: state.user.user,
 });
 
 const mapDispatchToProps = (dispatch: any): { actions: Actions } => ({
-    actions: {
-        logOutUser: () => dispatch(logOutUser()),
-    },
+  actions: {
+    logOutUser: () => dispatch(logOutUser()),
+  },
 });
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Header));
