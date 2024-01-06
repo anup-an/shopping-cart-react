acbebfc2eef856dfa9a48ae460539096cc04c32d
anup-an <friek_an@yahoo.com>
2024-01-06 03:56:26 +0200

    Update password reset flow

--- client/src/views/ResetPasswordPage/index.tsx
+++ client/src/views/ResetPasswordPage/index.tsx
@@ -5,21 +5,21 @@ import Header from '../../components/Header';
 import ResetPassword from '../../components/ResetPassword';
 
 class ResetPasswordPage extends React.Component {
-    render() {
-        return (
-            <div className="h-screen">
-                <div className="fixed w-full z-10">
-                    <Header focus="" />
-                </div>
-                <div className="h-full pt-20 flex justify-center items-center">
-                    <ResetPassword />
-                </div>
-                <div className="fixed bottom-0 lg:hidden w-full">
-                    <Footer focus="" />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="h-screen">
+        <div className="fixed w-full z-10">
+          <Header focus="" />
+        </div>
+        <div className="h-full pt-14 flex justify-center items-center">
+          <ResetPassword />
+        </div>
+        <div className="fixed bottom-0 lg:hidden w-full">
+          <Footer focus="" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ResetPasswordPage;
